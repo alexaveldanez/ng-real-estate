d84bb39737637e82c823579fb8ec842fbee8da6d
Alex Aveldanez <alexsaveldanez@gmail.com>
2022-02-14 19:53:58 -0800

    Added registration form
    Added registration form and user service

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
-  styleUrls: ['./add-property.component.scss']
+  styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropteryForm: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -15,6 +16,11 @@ export class AddPropertyComponent implements OnInit {
 
   onBack() {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('Congrats, form submitted!');
+    console.log(this.addPropteryForm)
   }
 
 }
